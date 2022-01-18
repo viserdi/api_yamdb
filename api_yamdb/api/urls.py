7a3c3d1882c3c8916d7e69836c78094ec4cf7305
AnnSonrisa <53565314+AnnSonrisa@users.noreply.github.com>
2022-01-18 21:48:01 +0300

    endpoints for categories, genres and titles

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import *
 
 router_v1 = DefaultRouter()
+router_v1.register('titles', TitleViewSet, basename='title')
+router_v1.register('categories', CategoryViewSet,
+                   basename='category')
+router_v1.register('genres', GenreViewSet, basename='genre')
 
 urlpatterns = [
     path('', include(router_v1.urls)),

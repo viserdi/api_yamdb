0c8f37652cf8b608767f7daf13efdd6b4248e549
AnnSonrisa <53565314+AnnSonrisa@users.noreply.github.com>
2022-01-18 19:15:54 +0300

    add the models title, category, genre

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.TextField('Название',
+                            blank=False,
+                            max_length=50)
+    slug = models.SlugField('slug',
+                            blank=False,
+                            unique=True,
+                            db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.TextField('Название',
+                            blank=False,
+                            max_length=50)
+    slug = models.SlugField('slug',
+                            blank=False,
+                            unique=True,
+                            db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.TextField(
+        'Название',
+        blank=False,
+        max_length=50,
+        db_index=True
+    )
+    year = models.IntegerField(blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles',
+        verbose_name='Категория'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        db_index=True,
+        related_name='titles',
+        verbose_name='Жанр'
+    )
+    description = models.CharField(
+        'Описание',
+        max_length=200,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+

2ecbe3f08ae537fcf69fa09553d5649625e32f39
AnnSonrisa <53565314+AnnSonrisa@users.noreply.github.com>
2022-01-18 21:54:22 +0300

    пагинация

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Category, Genre, Title
 from .serializers import (CategorySerializer, GenreSerializer,
@@ -9,15 +10,18 @@ from .serializers import (CategorySerializer, GenreSerializer,
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
+    pagination_class = PageNumberPagination
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by("id")
     serializer_class = CategorySerializer
     lookup_field = "slug"
+    pagination_class = PageNumberPagination
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all().order_by("id")
     serializer_class = GenreSerializer
     lookup_field = "slug"
+    pagination_class = PageNumberPagination

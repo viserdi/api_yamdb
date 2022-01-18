4c52d11615a18e7d03e98821f2dd020c766e80d4
AnnSonrisa <53565314+AnnSonrisa@users.noreply.github.com>
2022-01-18 21:36:23 +0300

    views TitleViewSet, CategoryViewSet, GenreViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Category, Genre, Title
+from .serializers import (CategorySerializer, GenreSerializer,
+                          TitleSerializer
+                          )
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by("id")
+    serializer_class = CategorySerializer
+    lookup_field = "slug"
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all().order_by("id")
+    serializer_class = GenreSerializer
+    lookup_field = "slug"

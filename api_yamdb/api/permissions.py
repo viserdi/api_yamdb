9913dd47285fcad808a16158de2009036539e3ed
Pavel Britvin <pashwf@gmail.com>
2022-01-19 22:02:41 +0300

    added superuser im permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -26,4 +26,4 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated
-                and request.user.is_admin)
+                and request.user.is_admin or request.user.is_superuser)

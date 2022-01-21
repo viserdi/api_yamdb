4da47fd4aaf71ac4662b0eb302977a0ebc5ff764
Иван <viserditech@yandex.ru>
2022-01-21 11:23:11 +0300

    permissions pep8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,33 +5,36 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS and
-                request.user.is_anonymous or
-                request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            and request.user.is_anonymous
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS or
-                obj.author == request.user or
-                request.user.is_admin or
-                request.user.is_moderator
-                )
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_admin
+            or request.user.is_moderator
+        )
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS or
-                (
-                        request.user.is_authenticated and
-                        request.user.is_admin
-                )
-                )
+            request.method in permissions.SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and request.user.is_admin
+            )
+        )
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_authenticated and
-                request.user.is_admin or request.user.is_superuser)
+        return (
+            request.user.is_authenticated
+            and request.user.is_admin
+            or request.user.is_superuser
+        )

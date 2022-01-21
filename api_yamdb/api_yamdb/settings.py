da8f9d6dc554641c4004be6ac3df81519f79832d
Иван <viserditech@yandex.ru>
2022-01-21 11:30:29 +0300

    +djoser

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'django_filters',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
